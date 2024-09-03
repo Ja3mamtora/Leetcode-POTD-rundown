9ec075d51ca08d639f042896b20b02eaad9c4541
Jatin Mamtora <91244035+Ja3mamtora@users.noreply.github.com>
2024-09-03 21:17:11 +0530

    Create 1945. Sum of Digits of String After Convert.java

---
+++ 1945. Sum of Digits of String After Convert.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int getLucky(String s, int k) {
+        String t = "";
+        for(int i = 0 ; i < s.length() ; i++) {
+            int d = s.charAt(i) - 'a'+1;
+            t += d;
+        }
+
+        int ans = 0;
+        while(k > 0) {
+            int sum = 0;
+            for(int i = 0 ; i < t.length() ; i++) {
+                sum += t.charAt(i)-'0';
+            }
+            ans = sum;
+            t = ""+sum;
+            k--;
+        }
+        return ans;
+    }
+}
